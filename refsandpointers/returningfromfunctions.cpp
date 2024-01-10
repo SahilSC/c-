0fdf99d94a627aed1cc50bf58e65c406c09b507e
Sahil <sahilschowdhury@gmail.com>
2024-01-09 19:40:42 -0600

    dangling pointer

---
+++ refsandpointers/returningfromfunctions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct Enemy {
+    int age;
+};
+
+Enemy* getRef() {
+    Enemy e;
+    return &e;
+}
+
+int main(){
+
+}#include <iostream>
+using namespace std;
+
+struct Enemy {
+    int age;
+    ~Enemy() {
+        cout << "Destructor!" << endl;
+    }
+};
+
+Enemy* getRef() {
+    Enemy e;
+    return &e;
+}
+
+int main(){
+    //danging pointer
+    Enemy enemy = *getRef();
+    cout << enemy.age << endl;
+}
