d961aeb225af7e82e5f72f332d894e617998412b
Sahil <sahilschowdhury@gmail.com>
2024-06-03 13:30:42 -0500

    finish quick select

--- algo/countingsort.cpp
+++ algo/countingsort.cpp
@@ -1,1 +1,83 @@
+/*
+ * Counting sort:
+ * Sort range quickly at cost of space
+ * N elements, range of M
+ * Time complexity : O(N+M) (avg, best, worst)
+ * Space complexity : O(N+M)
+ *
+ * 1) create array with size M+1 and compute a prefix sum of number of occurences
+ * 2) iterate backwards through input array and place in appropriate location in output array
+ */
+
+/*
+ * Tutorial: https://www.geeksforgeeks.org/counting-sort/
+ * Problem: https://leetcode.com/problems/kth-largest-element-in-an-array
+ */
+
+/*
+ * MISC
+ * Advantages:
+ * - Stable
+ * - Faster than merge
+ *
+ * Disadvantages:
+ * - Doesn't work for decimal vals/strings
+ * - Slow for large ranges
+ */
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> countSort(vector<int> &inputArray) {
+
+    int N = inputArray.size();
+
+    // Finding the maximum element of array inputArray[].
+    int M = 0;
+
+    for (int i = 0; i < N; i++)
+        M = max(M, inputArray[i]);
+
+    // Initializing countArray[] with 0
+    vector<int> countArray(M + 1, 0);
+
+    // Mapping each element of inputArray[] as an index
+    // of countArray[] array
+
+    for (int i = 0; i < N; i++)
+        countArray[inputArray[i]]++;
+
+    // Calculating prefix sum at every index
+    // of array countArray[]
+    for (int i = 1; i <= M; i++)
+        countArray[i] += countArray[i - 1];
+
+    // Creating outputArray[] from countArray[] array
+    vector<int> outputArray(N);
+
+    for (int i = N - 1; i >= 0; i--) {
+        outputArray[countArray[inputArray[i]] - 1]
+                = inputArray[i];
+
+        countArray[inputArray[i]]--;
+    }
+
+    return outputArray;
+}
+
+// Driver code
+int main() {
+
+    // Input array
+    vector<int> inputArray = {4, 3, 12, 1, 5, 5, 3, 9};
+
+    // Output array
+    vector<int> outputArray = countSort(inputArray);
+
+    for (int i = 0; i < inputArray.size(); i++)
+        cout << outputArray[i] << " ";
+
+    return 0;
+}
 
--- algo/quickselect.cpp
+++ algo/quickselect.cpp
@@ -1,1 +1,62 @@
+//TODO
+/*
+ * Quickselect/Hoare's algorithm:
+ * Find kth largest element in unordered list
+ * N = size of list
+ * Time complexity : O(N) avg, O(N^2) worst
+ * Space complexity : O(1)
+ *
+ * 1) Choose (random if you want really want to avoid O(N^2)) pivot in array
+ * 2) Partition relative to pivot into greater, mid, less.
+ * 3)
+ */
 
+/*
+ * Tutorial: https://www.geeksforgeeks.org/quickselect-algorithm/
+ * Problem: https://leetcode.com/problems/kth-largest-element-in-an-array
+ */
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int kthSmallest(int arr[], int lo, int hi, int k) {
+    if (hi == lo) return arr[lo];
+    int pivot = (lo + rand() % (hi - lo + 1));
+    int midNum = arr[pivot];
+
+    //Partitioning
+    for (int i = lo; i <= hi; i++) {
+        for (int j = lo; j <= hi; j++) {
+            cout << arr[j] << " ";
+        }
+        cout << endl;
+        if (i <= pivot && arr[i] > midNum) {
+            swap(arr[i], arr[pivot]);
+            pivot = i;
+        } else if (i > pivot && arr[i] <= midNum) {
+            swap(arr[pivot], arr[pivot + 1]);
+            if (i != pivot + 1) swap(arr[i], arr[pivot]);
+            pivot++;
+        }
+
+    }
+
+    int left = pivot - lo + 1;
+    if (k == left) return arr[pivot];
+    if (left >= k) {
+        return kthSmallest(arr, lo, pivot, k);
+    } else {
+        return kthSmallest(arr, pivot + 1, hi, k - left);
+    }
+}
+
+int main() {
+    int arr[] = {10, 4, 5, 8, 6, 11, 26};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 4;
+    cout << "K-th smallest element is "
+         << kthSmallest(arr, 0, n - 1, k);
+    return 0;
+}
